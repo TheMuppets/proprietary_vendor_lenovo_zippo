1c514b5d72bdacd32be22dc7ef3041226f9a65e8
Michael Bestas <mkbestas@gmail.com>
2023-08-24 07:32:19 +0300

    zippo: Add proprietary-firmware.txt
    Change-Id: Ib9e421f4d8e4f4a20318cb76ddedce9c44987cad

--- Android.mk
+++ Android.mk
@@ -3,3 +3,23 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),zippo)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,c47f5c49b7e61beb52bc147e840713f7eae77066)
+$(call add-radio-file-sha1-checked,radio/aop.img,ffdf62150624940e0e10976c60f2a42a4a8a5b23)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ad657562529d90f3b99590bd7072b58d49e79dfd)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,3715eb2499ed4088eb1fac233f0a006be91036d5)
+$(call add-radio-file-sha1-checked,radio/dsp.img,144b07528e20574fcb444aad5f6c906a20335b83)
+$(call add-radio-file-sha1-checked,radio/hyp.img,91acac9701554757b3aabc032674f87aea30d134)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,a4b295412e834badd8ec6e3c9e171c42e8fa674c)
+$(call add-radio-file-sha1-checked,radio/modem.img,d4716d72add9df0349bec1f17ae6f5cd30417424)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,934c25c25790a44868ea99ee8b9859b33eacf819)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,924a79eca5a171125b357a48828f885d1c15f110)
+$(call add-radio-file-sha1-checked,radio/splash.img,d7b354102d590480146455d127e90d29c5f96b78)
+$(call add-radio-file-sha1-checked,radio/storsec.img,c98747c575bde60e8e0e2c5e2db4ea8f4dffc248)
+$(call add-radio-file-sha1-checked,radio/tz.img,c13f3385411e5e09797b42df06e362c358a3ba8b)
+$(call add-radio-file-sha1-checked,radio/xbl.img,cb9bc12f01021ac2c41801ebf907c934f658d097)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,271a37e9d26a624cab71cd8d2bb3273e35088fbe)
+
+endif
