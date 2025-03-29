0d03a72b6751bd8e66b346fe1a741616bbd7eff9
Marc Bourgoin <themard85@gmail.com>
2025-03-29 10:30:47 -0600

    zippo: Drop 64bit audio blobs
    Change-Id: Ia171a08bc56894898a4de3e4beff41f49594f280

--- zippo-vendor.mk
+++ zippo-vendor.mk
@@ -193,9 +193,8 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/zippo/proprietary/vendor/lib64/camera/fdconfigvideolite.bin:$(TARGET_COPY_OUT_VENDOR)/lib64/camera/fdconfigvideolite.bin
 
 PRODUCT_PACKAGES += \
-    audio.primary.msmnile \
-    libclimax \
     vendor.qti.hardware.fingerprint@1.0 \
+    audio.primary.msmnile \
     vendor.qti.hardware.audiohalext@1.0-impl \
     liba2dpoffload \
     libacdb-fts \
@@ -206,6 +205,7 @@ PRODUCT_PACKAGES += \
     libaudcal \
     libaudio_log_utils \
     libaudioconfigstore \
+    libclimax \
     libqtigef \
     libssrec \
     libasphere \
